1a9ccdeca700cb99f66dc69d8fee584f5f6a805f
peeyushjss <peeyushjss@gmail.com>
2020-06-21 12:13:24 +0530

    Storybook project completed

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,30 @@
-const express = require("express");
-const { Router } = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-/*  @desc Login/Landing Page 
-    @router GET /
-*/
-router.get('/', (req, res) => {
-    res.render('login', {
-        layout: 'login'
-    });
-});
+const Story = require('../models/Story')
 
-/*  @desc Dashboard/Landing Page 
-    @router GET /dashboard
-*/
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
-});
+// @desc    Login/Landing page
+// @route   GET /
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
+})
 
-module.exports = router;
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+module.exports = router
